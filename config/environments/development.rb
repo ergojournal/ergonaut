113ec1e95f29a9c2702645498dde9074555d201c
Ergo Journal <a.j.wilson@bham.ac.uk>
2020-05-20 08:07:02 +0000

    Initialize AWS local repository

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,5 @@ Ergonaut::Application.configure do
   # Mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
-    
+
 end
